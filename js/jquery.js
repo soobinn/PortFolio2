4c1bf2ac168b66978449a4c2439054eff8ba73bc
soobinn <76994237+soobinn@users.noreply.github.com>
2023-07-25 17:26:30 +0900

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function () {
+
+
+
+
+
+    $('.logo li:nth-child(1)').on('click',function(){
+    $('html,body').animate({scrollTop:0},800)})
+
+    $('.logo li:nth-child(2)').on('click',function(){
+    $('html,body').animate({scrollTop:2200},800)})
+
+    $('.logo li:nth-child(3)').on('click',function(){
+    $('html,body').animate({scrollTop:2950},800)})
+
+    $('.logo li:nth-child(4)').on('click',function(){
+    $('html,body').animate({scrollTop:3730},800)})
+
+
+
+
+
+
+})
